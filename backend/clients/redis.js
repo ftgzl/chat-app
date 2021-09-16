ff261c82e4702fbb87f08c4447c755d67df78dc2
fuatguzel <fuatguzel754@gmail.com>
2021-09-16 16:54:02 +0300

    Add redis client

---
+++ backend/clients/redis.js
@@ -0,0 +1,12 @@
+const redis = require('redis')
+
+const getClient = () => {
+  return redis.createClient({
+    host: 'localhost',
+    port: 6379,
+    password: '',
+    db: 1
+  })
+}
+
+module.exports.getClient = getClient
